84fd066925f5ea504b872db3eaff9474a4d9fc98
Andrey <andrey149502@gmail.com>
2016-11-23 20:33:32 +0200

    page 40

--- app.ts
+++ app.ts
@@ -6,26 +6,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 @Component({
-	selector: 'hello-world',
+	selector: 'reddit',
 	template: `
-	<div>
-		Hello {{ name }}
-	</div>	
+		<form class="ui large form segment">
+			<h3 class="ui header">Add a Link</h3>
+
+			<div class="field">
+				<label for="title">Title:</label>
+				<input name="title" #newtitle>
+			</div>
+			<div class="field">
+				<label for="link">Link:</label>
+				<input name="link" #newlink>
+			</div>
+
+			<button (click)="addArticle(newtitle, newlink)"
+							class="ui positive right floated button">
+				Submit link
+			</button>
+		</form>
+
+		<div class="ui grid posts">
+			<reddit-article></reddit-article>
+		</div>
 	`
 })
-class HelloWorld {
-	name: string;
-
-	constructor() {
-		this.name = 'Felipe';
+class RedditApp {
+	addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+		console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+		return false;
 	}
 }
 
 @NgModule({
-	declarations: [HelloWorld],
+	declarations: [ 
+		RedditApp
+	],
 	imports: [BrowserModule],
-	bootstrap: [HelloWorld],
+	bootstrap: [ RedditApp ],
 })
-class HelloWorldAppModule {}
+class RedditAppModule {}
 
-platformBrowserDynamic().bootstrapModule(HelloWorldAppModule);
+platformBrowserDynamic().bootstrapModule(RedditAppModule);
