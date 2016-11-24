75df76f89c777a3d2fb4972328c7e5d6708dbd80
Andrey <andrey149502@gmail.com>
2016-11-24 23:40:55 +0200

    page 60

--- app.ts
+++ app.ts
@@ -5,6 +5,89 @@ import {
 import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+
+
+class Article {
+	title: string;
+	link: string;
+	votes: number;
+
+	constructor(title: string, link: string, votes?: number) {
+		this.title = title;
+		this.link = link;
+		this.votes = votes || 0;
+	}
+
+	voteUp(): void {
+		this.votes += 1;
+	}
+		
+	voteDown(): void {
+		this.votes -= 1;
+	}
+
+	get domain(): string {
+		try {
+			const link: string = this.link.split('//')[1];
+			return link.split('/')[0];
+		} catch (err) {
+			return null;
+		}
+	}
+}
+
+@Component({
+	selector: 'reddit-article',
+	inputs: ['article'],
+	host: {
+		class: 'row'
+	},
+	template: `
+			<div class="four wide column center aligned votes">
+				<div class="ui statistic">
+					<div class="value">
+						{{ article.votes }}
+					</div>
+					<div class="label">
+						Points
+					</div>
+				</div>
+			</div>
+			<div class="twelve wide column">
+				<a class="ui large header" href="{{ article.link }}">
+					{{ article.title }}
+				</a>
+				<div class="meta">{{ article.domain }}</div>
+				<ul class="ui big horizontal list voters">
+					<li class="item">
+						<a href (click)="voteUp()">
+							<i class="arrow up icon"></i>
+							upvote
+						</a>
+					</li>
+					<li class="item">
+						<a href (click)="voteDown()">
+							<i class="arrow down icon"></i>
+							downvote
+						</a>
+					</li>
+				</ul>
+			</div>
+	`
+})
+class ArticleComponent {
+	article: Article;
+
+	voteUp(): boolean {
+		this.article.voteUp();
+		return false;
+	}
+
+	voteDown(): boolean {
+		this.article.voteDown();
+		return false;
+	}
+}
 @Component({
 	selector: 'reddit',
 	template: `
@@ -27,7 +110,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 		</form>
 
 		<div class="ui grid posts">
-			<reddit-article></reddit-article>
+			<reddit-article
+				*ngFor="let article of sortedArticles()"
+				[article]="article">
+			</reddit-article>
 		</div>
 	`
 })
@@ -41,95 +127,17 @@ class RedditApp {
 			new Article('Angular Homepage', 'http://angular.io', 1),
 		];
 	}
-	
+
 	addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
 		console.log(`Adding article title: ${title.value} and link: ${link.value}`);
-		return false;
-	}
-}
-
-
-class Article {
-	title: string;
-	link: string;
-	votes: number;
-
-	constructor(title: string, link: string, votes?: number) {
-		this.title = title;
-		this.link = link;
-		this.votes = votes || 0;
-	}
-
-	voteUp(): void {
-		this.votes += 1;
-	}
-
-	voteDown(): void {
-		this.votes -= 1;
-	}
-
-	domain(): string {
-		try {
-			const link: string = this.link.split('//')[1];
-			return link.split('/')[0];
-		} catch (err) {
-			return null;
-		}
-	}
-}
-
-@Component({
-	selector: 'reddit-article',
-	host: {
-		class: 'row'
-	},
-	template: `
-			<div class="four wide column center aligned votes">
-				<div class="ui statistic">
-					<div class="value">
-						{{ article.votes }}
-					</div>
-					<div class="label">
-						Points
-					</div>
-				</div>
-			</div>
-			<div class="twelve wide column">
-				<a class="ui large header" href="{{ article.link }}">
-					{{ article.title }}
-				</a>
-				<ul class="ui big horizontal list voters">
-					<li class="item">
-						<a href (click)="voteUp()">
-							<i class="arrow up icon"></i>
-							upvote
-						</a>
-					</li>
-					<li class="item">
-						<a href (click)="voteDown()">
-							<i class="arrow down icon"></i>
-							downvote
-						</a>
-					</li>
-				</ul>
-			</div>
-	`
-})
-class ArticleComponent {
-	article: Article;
-
-	constructor() {
-		this.article = new Article('Angular 2', 'http://angular.io', 10);
-	}
-
-	voteUp(): boolean {
-		this.article.voteUp();
+		this.articles.push(new Article(title.value, link.value, 0));
+		title.value = '';
+		link.value = '';
 		return false;
 	}
 
-	voteDown(): boolean {
-		this.article.voteDown();
-		return false;
+	sortedArticles(): Article[] {
+		return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
 	}
 }
 
